1b9314c7bad64f8fc89be67a17bb15ab2e70ccbd
Krista Blachian <krista.blachian@scifusion360.com>
2019-06-03 14:21:13 -0400

    added units methods

--- src/com/theironyard/Board/Board.java
+++ src/com/theironyard/Board/Board.java
@@ -26,4 +26,27 @@ public class Board {
             }
         }
     }
+
+    public Tile getTile(int x, int y) {
+        return (Tile) ((ArrayLIst)tiles.get(x-1)).get(y-1);
+    }
+
+    public void addUnit(Unit unit, int x, int y) {
+        Tile tile = getTile(x, y);
+        tile.addUnit(unit);
+    }
+
+    public void removeUnit(Unit unit, int x, int y) {
+        Tile tile = getTile(x, y);
+        tile.removeUnit(unit);
+    }
+
+    public void removeUnits(int x, int y) {
+        Tile tile = getTile(x, y);
+        tile.removeUnits();
+    }
+
+    public List getUnits(int x, int y) {
+        return getTile(x, y).getUnits();
+    }
 }
